216911a2ceeb6a5ef485209cfd1631a1ec776895
Jack Collier <steamcow12@hotmail.com>
2024-10-23 01:12:16 +0100

    add valentine's day countdown and fix day rounding issue

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 function calculateDaysUntilTargetDate(elementId, targetDate) {
     const currentDate = new Date();
+
+    currentDate.setHours(0, 0, 0, 0); 
+    targetDate.setHours(0, 0, 0, 0);
+    
     const daysRemaining = Math.round((targetDate - currentDate) / (1000 * 60 * 60 * 24));
 
     const targetElement = document.getElementById(elementId);
@@ -10,3 +14,4 @@ function calculateDaysUntilTargetDate(elementId, targetDate) {
 
 calculateDaysUntilTargetDate("countdown-christmas", new Date("2024-12-25"));  
 calculateDaysUntilTargetDate("countdown-newyear", new Date("2025-01-01"));  
+calculateDaysUntilTargetDate("countdown-valentines", new Date("2025-02-14"));
