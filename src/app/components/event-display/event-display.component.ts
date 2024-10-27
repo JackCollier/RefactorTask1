b405d951781df74f4c0f3ea0e6befd4b7a000870
Jack Collier <steamcow12@hotmail.com>
2024-10-27 21:34:38 +0000

    create event service and implement in EventDisplay

--- src/app/components/event-display/event-display.component.ts
+++ src/app/components/event-display/event-display.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Event, EventService } from '../../services/event.service';
 
 @Component({
     selector: 'event-display',
     templateUrl: 'event-display.component.html'
 })
 
-export class EventDisplay  {
-  
+export class EventDisplay implements OnInit {
+  events: Event[] = [];
+
+  constructor(private eventService: EventService) {}
+
+  ngOnInit() {
+    this.eventService.events$.subscribe(events => {
+      this.events = events;
+    });
+  }
 }
--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 
 export interface Event {
@@ -10,4 +11,16 @@ export interface Event {
   providedIn: 'root'
 })
 export class EventService {
+    private eventsSubject = new BehaviorSubject<Event[]>([]);
+    events$ = this.eventsSubject.asObservable();
+  
+    addEvent(event: Event) {
+      const currentEvents = this.eventsSubject.getValue();
+      this.eventsSubject.next([...currentEvents, event]);
+    }
+  
+    removeEvent(eventToRemove: Event) {
+      const updatedEvents = this.eventsSubject.getValue().filter(event => event !== eventToRemove);
+      this.eventsSubject.next(updatedEvents);
+    }
 }
