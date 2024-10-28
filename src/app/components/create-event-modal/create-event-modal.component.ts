8de510590b28c66a160c1e2d57972c60c2bc42d2
Jack Collier <steamcow12@hotmail.com>
2024-10-28 01:48:35 +0000

    implement async pipe show modal check and improve form validity

--- src/app/components/create-event-modal/create-event-modal.component.ts
+++ src/app/components/create-event-modal/create-event-modal.component.ts
@@ -1,36 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { Event, EventStateService } from '../../services/state/event-state.service';
+import {
+  Event,
+  EventStateService,
+} from '../../services/state/event-state.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'create-event-modal',
-    templateUrl: 'create-event-modal.component.html',
-    standalone: true,
-    imports: [FormsModule, CommonModule]
+  selector: 'create-event-modal',
+  templateUrl: 'create-event-modal.component.html',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
 })
-
-export class CreateEventModalComponent implements OnInit  {
-  showModal : boolean = false;
+export class CreateEventModalComponent {
   newEvent: Event = { name: '', date: '' };
 
-  constructor(private eventService: EventStateService) {}
-
-  ngOnInit() {
-    this.eventService.showModal$.subscribe(isVisible => {
-      this.showModal = isVisible;
-    });
-  }
+  constructor(public eventStateService: EventStateService) {}
 
   addEvent() {
     if (this.newEvent.name && this.newEvent.date) {
-      this.eventService.addEvent(this.newEvent);
+      this.eventStateService.addEvent(this.newEvent);
       this.newEvent = { name: '', date: '' };
-      this.eventService.closeModal();
+      this.eventStateService.closeModal();
     }
   }
 
   closeModal() {
-    this.eventService.closeModal();
+    this.eventStateService.closeModal();
   }
 }
